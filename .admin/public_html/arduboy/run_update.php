f6ea984aa7f208186ab78fec2031a083103ac871
Erwin Ried <erwinried@gmail.com>
2018-03-05 14:26:47 +0100

    Using a relative path for the template

--- .admin/public_html/arduboy/run_update.php
+++ .admin/public_html/arduboy/run_update.php
@@ -6,12 +6,12 @@ if($s!=$secret_keywords) // Prevent abuse
 
 if(!file_exists('pending_run'))
   die('Exited');
-  
+
 echo 'Starting update';
 unlink('pending_run');
 
 /* gets the data from a URL */
-function get_data($url) 
+function get_data($url)
 {
 	$ch = curl_init();
 	$timeout = 30;
@@ -25,17 +25,17 @@ function get_data($url)
 }
 
 // UTF8 Encode
-function utf8ize($d) 
+function utf8ize($d)
 {
-    if (is_array($d)) 
-        foreach ($d as $k => $v) 
+    if (is_array($d))
+        foreach ($d as $k => $v)
             $d[$k] = utf8ize($v);
 
      else if(is_object($d))
-        foreach ($d as $k => $v) 
+        foreach ($d as $k => $v)
             $d->$k = utf8ize($v);
 
-     else 
+     else
         return utf8_encode($d);
 
     return $d;
@@ -51,14 +51,14 @@ foreach($repo->tree as $f)
 {
   $info = pathinfo($f->path);
   @$s = $info["extension"];
-  
+
   if(strlen($s)>0)
   {
-  
+
     // Special case for source.url
     if($info["filename"] == 'source')
       $s='sourceurl';
-      
+
     $path = $info["dirname"] /*. "/" . $info["filename"]*/;
     $output[$path][strtolower($s)] = $f->path;
   }
@@ -70,7 +70,7 @@ foreach($repo->tree as $f)
 function get_url($s)
 {
   $url = '';
-  
+
   if(strlen($s)>0)
   {
     $ss = get_data(g($s));
@@ -84,13 +84,13 @@ function get_url($s)
 function g($p)
 {
   global $server, $repopath;
-  
+
   $p=rawurlencode($p);
   return "{$server}{$repopath}master/{$p}";
 }
 
 // URL exist, from: http://www.barattalo.it/coding/test-if-a-remote-url-exists-with-php-and-curl/
-function url_exists($url) 
+function url_exists($url)
 {
     $h = get_headers($url);
     $status = array();
@@ -109,76 +109,76 @@ foreach($output as $k => $f)
     $info = pathinfo($k);
     $title = $info["basename"];
     @$bin = (string)($output[$k]['hex']);
-    
+
     //echo $k . ' value: ' . $f;
-    /*?><pre><?php print_r($output[$k]['hex']); ?></pre><?php // */ 
-    
+    /*?><pre><?php print_r($output[$k]['hex']); ?></pre><?php // */
+
     if(strlen($bin)>0)
-    {  
+    {
       $genre = $info["dirname"];
       @$s = $output[$k]['txt'];
       @$description = strlen($s)>0?get_data(g($s)):'';
-      
+
       // Get other data
       $author = 'Unknown';
       $modified = '2017-01-01T12:00:00Z';
-      $license = 'Unspecified';
-      
+      $license = '';
+
       @$s = $output[$k]['ini'];
-      
+
       if(strlen($s)>0)
       {
         $ini = parse_ini_string(get_data(g($s)),FALSE,INI_SCANNER_RAW);
 
         if(strlen($ini['author'])>0)
           $author = $ini['author'];
-          
+
         if(strlen($ini['date'])>0)
           $modified = $ini['date'];
-        
-        // Remote hex location  
+
+        // Remote hex location
         if(@strlen($ini['hex'])>0)
         {
           if(url_exists($ini['hex']))
             $bin = $ini['hex'];
         }
-        
+
         if(strlen($ini['description'])>0)
           $description = $ini['description'];
-        
-        if(strlen($ini['title'])>0)
+
+        if(@strlen($ini['title'])>0)
           $title = $ini['title'];
-          
+
         if(@strlen($ini['license'])>0)
           $license = $ini['license'];
       }
 
       // Get URL
-      $url = get_url($output[$k]['url']);
-      $sourceUrl = get_url($output[$k]['sourceurl']);
-      
+      @$url = get_url($output[$k]['url']);
+      @$sourceUrl = get_url($output[$k]['sourceurl']);
+
       if(strlen($sourceUrl)==0)
         $sourceUrl = $url;
-      
+
       $screenshots = array();
       $hex = array(array("title"=>$title,"filename"=>g($bin),"device"=>"Arduboy"));;
-      
+
       @$s = $output[$k]['png'];
       $banner = strlen($s)>0?g($s):'';
-      
+
       if(strlen($banner)<=3)
       {
         @$s = $output[$k]['jpg'];
         $banner = strlen($s)>0?g($s):'';
       }
-      
+
       $names[] = $title;
       $files[] = array("title"=>trim($title),
       "description"=>trim($description),
       "genre"=>$genre,"url"=>$url,"sourceUrl"=>$sourceUrl,"author"=>$author,"banner"=>$banner,"date"=>$modified,"binaries"=>$hex,"screenshots"=>$screenshots,"license"=>$license, "arduboy"=>"");
     }
   }
-} 
+}
 
 echo '<br>Writing results';
 
@@ -186,7 +186,7 @@ echo '<br>Writing results';
 array_multisort($names,SORT_STRING | SORT_FLAG_CASE,$files);
 
 $values = array("repository"=>"Erwin's Arduboy Collection", "api-version"=>"1.0", "email"=>"", "maintainer"=>"Erwin Ried","website"=>"http://ried.cl","items"=>array_values($files));
-file_put_contents("repo.json",json_encode(utf8ize($values),JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_PRETTY_PRINT)); 
+file_put_contents("repo.json",json_encode(utf8ize($values),JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_PRETTY_PRINT));
 
 // Mode debug
 echo '<br>json encode result: ';
@@ -205,8 +205,7 @@ echo 'Creating website';
 require 'Mustache/Autoloader.php';
 Mustache_Autoloader::register();
 
-$template = file_get_contents("http://arduboy.ried.cl/template.html");
-//$values = json_decode(file_get_contents("http://arduboy.ried.cl/repo.json"));
+$template = file_get_contents("template.html");
 
 //start the mustache engine
 $m = new Mustache_Engine;
