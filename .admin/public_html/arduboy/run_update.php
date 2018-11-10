24829c7bf5a8cd27f5c7cc06e9375702a0d09e7f
Erwin Ried <1091420+eried@users.noreply.github.com>
2018-11-10 01:37:22 +0100

    Simple rating system

--- .admin/public_html/arduboy/run_update.php
+++ .admin/public_html/arduboy/run_update.php
@@ -183,7 +183,7 @@ foreach($output as $k => $f)
       $names[] = $title;
       $files[] = array("title"=>trim($title),
       "description"=>trim($description),
-      "genre"=>$genre,"url"=>$url,"sourceUrl"=>$sourceUrl,"author"=>$author,"banner"=>$banner,"date"=>$modified,"binaries"=>$hex,"screenshots"=>$screenshots,"license"=>$license, "arduboy"=>"");
+      "genre"=>$genre,"url"=>$url,"sourceUrl"=>$sourceUrl,"author"=>$author,"banner"=>$banner,"date"=>$modified,"binaries"=>$hex,"screenshots"=>$screenshots,"license"=>$license,"id"=>md5($author . '_' . $title), "arduboy"=>"");
     }
   }
 }
