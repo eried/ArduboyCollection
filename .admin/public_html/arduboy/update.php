bd079f309539b7d054e5786805b1b883533eb8bd
Erwin Ried <1091420+eried@users.noreply.github.com>
2019-03-07 02:23:41 +0100

    Preparing github pages for the repo

--- .admin/public_html/arduboy/update.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'config.php';
-@$s=$_GET['secret'];
-if($s!=$secret_keywords) // Prevent abuse
-  die('Not available');
-
-file_put_contents('pending_run','1');
-echo 'Update requested';
